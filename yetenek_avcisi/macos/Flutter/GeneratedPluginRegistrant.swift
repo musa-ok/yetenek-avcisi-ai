45f852415cf753183b9b16d812ad7cec12689305
Musa Ok <musaok@Musa-MacBook-Air.local>
2026-05-23 00:09:28 +0300

    chore: bump build number to 5 for App Store submission

--- yetenek_avcisi/macos/Flutter/GeneratedPluginRegistrant.swift
+++ yetenek_avcisi/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus
 import file_picker
 import file_selector_macos
 import google_sign_in_ios
@@ -16,7 +15,6 @@ import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
